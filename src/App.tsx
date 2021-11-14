ae8dac31cd11f486aa259a909de641bf93272778
makcsholonik <makcsholonik@gmail.com>
2021-11-14 22:46:48 +0300

    14.11.21

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 function App () {
-	return (
-		<div className="App">
 
+	const [count, setCount] = useState<number> ( 0 );
+
+	const decrement = () => setCount ( count - 1 );
+	const increment = () => setCount ( count + 1 );
+
+
+
+	return (
+		<div className="app">
+			<h1 className={count > 0 ? 'positive' : count < 0 ? 'negative' : ''}>{ count }</h1>
+			<div className="button__wrapper">
+				<button onClick={ decrement }>-</button>
+				<button onClick={ increment }>+</button>
+			</div>
 		</div>
 	);
 }
